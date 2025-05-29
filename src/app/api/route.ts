24fc2d56d0605933524c09cd0457d93fa6380bdc
Danilo Carrabino <d.carrabino@factor-eleven.com>
2025-05-29 03:12:01 +0200

    Chore: modified the root route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,3 @@
 export async function GET() {
-  return Response.json({ message: 'Welcome to the Random API in Next.js' })
+  return Response.json({ message: 'Welcome to the Random APIs! Powered by Next.js' })
 }
